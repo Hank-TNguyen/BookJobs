f620177f26cc1dc6332f6264b78e1ce9c4672704
jwong <jwongzy@ymail.com>
2016-10-01 22:53:45 +0800

    Upload book UI draft v1

---
+++ app/src/main/java/bookjobs/bookjobs/UploadBookActivity.java
@@ -0,0 +1,16 @@
+package bookjobs.bookjobs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class UploadBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_book);
+
+    }
+
+}
